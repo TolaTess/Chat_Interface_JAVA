68cf1841769a69d213112cbf826aa73bfc34aff4
Tola Otesanya <tolaotesanya@Tolas-MacBook-Pro.local>
2018-03-13 15:59:49 +0000

    chat interface

---
+++ src/chat/Chat.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chat;
+
+/**
+ *
+ * @author tolaotesanya
+ */
+public class Chat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        /*
+        Chat_GUI chat = new Chat_GUI();
+        chat.setVisible(true);
+        
+        Chat2_GUI chat2 = new Chat2_GUI();
+        chat2.setVisible(true);
+        */
+    }
+    
+}
